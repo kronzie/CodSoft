7a211a7d898b17413f610ff64bdbd6813a2b14cf
kronzie <vyshakhnambiar11@gmail.com>
2025-01-06 22:37:20 +0530

    Complete basic styling and setup

---
+++ 03 calculator/script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    display = document.querySelector('.display');
+    key1 = document.querySelector('.key1');
+    key2 = document.querySelector('.key2');
+    key3 = document.querySelector('.key3');
+    key4 = document.querySelector('.key4');
+    key5 = document.querySelector('.key5');
+    key6 = document.querySelector('.key6');
+    key7 = document.querySelector('.key7');
+    key8 = document.querySelector('.key8');
+    key9 = document.querySelector('.key9');
+    key0 = document.querySelector('.key0');
+    keyPLUS = document.querySelector('.key-plus');
+    keyMINUS = document.querySelector('.key-minus');
+    keyMULTIPLY = document.querySelector('.key-multiply');
+    keyDIVIDE = document.querySelector('.key-divide');
+    keyDOT = document.querySelector('.key-dot');
+    keyDEL = document.querySelector('.keyDEL');
+    keyRESET = document.querySelector('.keyRESET');
+    keyEQUAL = document.querySelector('.key-equal');
+    
+    display.value = "3";
+}
+
+
+
