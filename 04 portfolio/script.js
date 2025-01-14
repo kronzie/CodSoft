6f37a1adc42dda88ec80abef8b02f538da2162dd
gameDevKronzie <gamedevkronzie@gmail.com>
2025-01-14 16:20:17 +0530

    Navbar fs done ✅

---
+++ 04 portfolio/script.js
@@ -0,0 +1,29 @@
+// const currentPage = window.location.pathname;
+
+// const homeIconNotActive = document.querySelector(".bi-house");
+// const homeIconActive = document.querySelector(".bi-house-fill");
+
+// const projectIconNotActive = document.querySelector(".bi-folder");
+// const projectIconActive= document.querySelector(".bi-folder-fill");
+
+// const toolsIconNotActive = document.querySelector(".bi-nut");
+// const toolsIconActive = document.querySelector(".bi-nut-fill");
+
+// const targetHomePage = "/index.html";
+// const targetProjectsPage = "/pages/projects.html";
+// const targetToolsPage = "/pages/tools.html";
+
+// if(currentPage === targetHomePage) {
+//     homeIconNotActive.classList.add("hidden");
+//     homeIconActive.classList.remove("hidden");
+// } 
+
+// if (currentPage === targetProjectsPage) {
+//     projectIconNotActive.classList.add("hidden");
+//     projectIconActive.classList.remove("hidden");
+// }
+
+// if(currentPage === targetToolsPage) {
+//     toolsIconNotActive.classList.add("hidden");
+//     toolsIconActive.classList.remove("hidden");
+// }
